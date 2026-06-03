0655d582da709ab49bf5583d665d63afcc7df001
Leo Young <gzyangle2018@gmail.com>
2026-06-03 11:21:50 +0800

    Update frontend/src/lib/utils.ts

---
+++ frontend/src/lib/utils.ts
@@ -0,0 +1,21 @@
+import clsx from 'clsx'
+
+export function cn(...inputs: (string | undefined | false | null)[]) {
+  return clsx(inputs)
+}
+
+export function formatCurrency(value: number): string {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  }).format(value)
+}
+
+export function formatNumber(value: number): string {
+  return new Intl.NumberFormat('en-US').format(value)
+}
+
+export function formatPercent(value: number): string {
+  return `${value.toFixed(2)}%`
+}
